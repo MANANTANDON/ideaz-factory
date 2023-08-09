d562860794a59a9deed55d5f539d6debb75bf855
MANAN TANDON <manantandon@gmail.com>
2023-08-09 17:14:07 +0530

    need to develop using Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import Marquee from "react-fast-marquee";
 import "./App.module.css";
+import { Carousel } from "./Components/Carousel/Carousel";
 import { Header } from "./Components/Header/Header";
+import { Section } from "./Components/Section/Section";
 
 function App() {
   return (
     <main>
       <Header />
+      <secion>
+        <Carousel />
+        <Marquee
+          speed={100}
+          style={{
+            background: "#f6454977",
+            height: "30px",
+            zIndex: "-20px",
+            fontSize: "12px",
+          }}
+        >
+          elusmod temp Lorem ipsum dolor sit amet., consectetur adipiscing elit,
+          sed do elusmod temp Lorer ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod temp Lorem ipsum dolor sit amet, consectetur
+          adipiscin ar 0081 DOBL 0002 0012 OOZE DOEZ OOPS 0052 0092 8 Lorem
+        </Marquee>
+        <Section />
+        <Section />
+      </secion>
     </main>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //files import
 import headercss from "./Header.module.css";
@@ -10,17 +10,29 @@ import logo from "../../Assets/logo.png";
 import { Search } from "@mui/icons-material";
 
 export const Header = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <React.Fragment>
       <header className={headercss.hc1}>
         <div className={headercss.hc2}>
-          <img src={logo} alt="news-arena-india" />
+          <img
+            src={logo}
+            alt="news-arena-india"
+            className={headercss.imageCont}
+          />
         </div>
         <div className={headercss.hc3}>
-          <input type="text" placeholder="Search your topics" />
+          <input
+            type="text"
+            placeholder="Search your topics"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
           <Search
             className={headercss.searchIcon}
             style={{ color: "#f64549" }}
+            onClick={() => console.log(search)}
           />
           <button className={headercss.hc4}>Sign Up</button>
           <button className={headercss.hc5}>Login</button>
