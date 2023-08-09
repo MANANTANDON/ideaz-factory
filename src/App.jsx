a05f32bb3c080effc14766a14a6cdd5396c95db3
MANAN TANDON <manantandon@gmail.com>
2023-08-09 13:55:46 +0530

    header make

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.module.css";
 import { Header } from "./Components/Header/Header";
 
 function App() {
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
+//files import
+import headercss from "./Header.module.css";
+
+//assets import
+import logo from "../../Assets/logo.png";
+
+//material ui import
+import { Search } from "@mui/icons-material";
+
 export const Header = () => {
-  return <React.Fragment>Header</React.Fragment>;
+  return (
+    <React.Fragment>
+      <header className={headercss.hc1}>
+        <div className={headercss.hc2}>
+          <img src={logo} alt="news-arena-india" />
+        </div>
+        <div className={headercss.hc3}>
+          <input type="text" placeholder="Search your topics" />
+          <Search
+            className={headercss.searchIcon}
+            style={{ color: "#f64549" }}
+          />
+          <button className={headercss.hc4}>Sign Up</button>
+          <button className={headercss.hc5}>Login</button>
+        </div>
+      </header>
+    </React.Fragment>
+  );
 };
